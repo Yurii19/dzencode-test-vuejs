251d1644df47fac0a3749f2e13eecfebdc28c194
Yurii19 <yurikolechkin@gmail.com>
2023-03-27 21:08:33 +0300

    chore: added top-menu component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
+  <TopMenu />
   <img alt="Vue logo" src="./assets/logo.png">
   <button type="button" class="btn btn-primary">Primary</button>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import TopMenu from './components/TopMenu.vue'
+//import HelloWorld from './components/TopMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopMenu,
+    // HelloWorld,
   }
 }
 </script>
@@ -23,6 +27,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/TopMenu.vue
@@ -0,0 +1,48 @@
+<template lang="html">
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#" style="color: #7cb342">
+      <img
+        src="../assets/protection.png"
+        width="30"
+        height="30"
+        class="d-inline-block align-top"
+        alt=""
+      />
+      Inventory
+    </a>
+    <div class="input-group w-25">
+      <!-- <span class="input-group-text" id="basic-addon1">@</span> -->
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Username"
+        aria-label="Username"
+        aria-describedby="basic-addon1"
+      />
+    </div>
+    <div>
+      <span>{{ today }}</span>
+      <span>{{ cDate }}<ClockIcon/></span>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import ClockIcon from '@/icons/ClockIcon.vue'
+
+const currentDate = new Date();
+const weekday = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+const today = weekday[currentDate.getDay()];
+// const cDate = currentDate;
+
+</script>
+
+<style scoped></style>
